8ebad6bf54b7040eeda9640fee49f360d6ad4d46
radenbesus2015-web <radenbesus2015@gmail.com>
2025-11-06 11:10:42 +0700

    repo size

--- FunMeter/FaceRecognition/src-vue-original/i18n/messages.js
+++
@@ -1,9 +0,0 @@
-import id from "./locales/id.json";
-import en from "./locales/en.json";
-
-export const messages = { id, en }
-
-export const LANGUAGE_OPTIONS = [
-  { code: "id", label: "Bahasa Indonesia" },
-  { code: "en", label: "English" },
-];
